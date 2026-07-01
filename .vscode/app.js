d4df0e5fb55f323e7f23371737697a2fcf516c33
diego-kelly <kellydiego927@gmail.com>
2026-07-01 10:47:37 +0000

    complicado

---
+++ .vscode/app.js
@@ -0,0 +1,18 @@
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+} 
+const sum = (a,b) => {
+    return a + b 
+}
+console.log(sum(7,3))
+
+module.exports = {sum, fromEuroToDollar};
+function fromDollarToYen(dollars){
+
+}
+function fronYenToPound(yens){
+    
+}
